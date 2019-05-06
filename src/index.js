64e3d2cec90ae1f10b12a33505b8ecdd266776a7
Bartlomiej Ploszynski <bartlomiej.ploszynski@contractors.roche.com>
2019-05-06 09:48:10 +0200

    Apollo Setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const userCredentials = { firstname: 'Robin' };
 const userDetails = { nationality: 'German' };
@@ -7,6 +9,39 @@ const user = {
   ...userCredentials,
   ...userDetails,
 };
+
+const app = express();
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
 
 console.log(user);
 
