514724b0b0da34b98e7f8d4338f4ce11e1072c66
Bartlomiej Ploszynski <bartlomiej.ploszynski@contractors.roche.com>
2019-05-06 10:19:26 +0200

    Apollo context and default values.

--- src/index.js
+++ src/index.js
@@ -30,10 +30,14 @@ let users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
+    firstName: 'Robin',
+    lastName: 'Wieruch',
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    firstName: 'Dave',
+    lastName: 'Davids',
   },
 };
 
@@ -47,18 +51,24 @@ const resolvers = {
     user: (parent, { id }) => {
       return users[id];
     },
-    me: () => {
+    me: (parent, args, { me }) => {
       return me;
     },
     students: () => {
       return studentsArray;
     },
   },
+  User: {
+    username: user => `${user.firstName} ${user.lastName}`,
+  },
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
